fbca9cb931687ceae01484d0579ef43dd8bef873
Elliot Lewis <elliotleelewis@gmail.com>
2018-10-15 12:34:51 +0100

    Fixed Unit Test

--- ClientApp/src/components/layout.test.tsx
+++ ClientApp/src/components/layout.test.tsx
@@ -1,12 +1,12 @@
+import { shallow } from 'enzyme';
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 
 import Layout from './layout';
 
 describe('Layout', () => {
 	it('renders without crashing', () => {
-		const div = document.createElement('div');
-		ReactDOM.render(<Layout />, div);
-		ReactDOM.unmountComponentAtNode(div);
+		shallow(
+			<Layout />,
+		)
 	});
 });
