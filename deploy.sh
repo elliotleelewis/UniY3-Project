5deede0a25c9e25dbd7d7bd9d95a8c8e40c84e06
Elliot Lewis <elliotleelewis@gmail.com>
2019-02-20 04:31:12 +0000

    Deployment Test

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
-echo $HOME
-uname -a
-pwd
+
+PROJECT=UniY3-Project
+
+cd ./projects
 ls
+if [ ! -d "$PROJECT" ]; then
+	git clone "https://github.com/elliotleelewis/$PROJECT.git"
+fi
+cd $PROJECT
+git pull
+docker-compose build
+docker-compose down
+docker-compose up -d
